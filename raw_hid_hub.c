7ef8af6559738429e41402ff599a39ed3a02377f
Morgan Sun <morgan.newell.sun@gmail.com>
2024-09-22 23:19:15 -0400

    remove unnecessary includes

--- raw_hid_hub.c
+++ raw_hid_hub.c
@@ -20,13 +20,7 @@
 #    include <windows.h>
 #else
 #    include <errno.h>
-#    include <fcntl.h>
 #    include <pthread.h>
-#    include <semaphore.h>
-#    include <sys/types.h>
-#    include <sys/wait.h>
-#    include <time.h>
-#    include <unistd.h>
 #endif
 
 // ============================================================================
